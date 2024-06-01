7c7d51e62510b5b54273eccddc70d24bf184d3bb
yangjiale <yangjiale@njupt.com>
2024-06-01 18:29:10 +0800

    git 1

---
+++ src/main/java/com/njupt/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.njupt.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+    }
+}
